2158276229d5dd4a2866d0a8005da13b970f476f
senhorbento <sbento.ti@gmail.com>
2022-06-19 06:47:26 -0700

    Feat: Add EntidadeCep

---
+++ consumindo-api-rest-csharp/API/EntidadeCep.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace consumindo_api_rest_csharp.API
+{
+    public class EntidadeCep
+    {
+        [JsonProperty("cep")]
+        public string cep               { get; set; }
+        [JsonProperty("logradouro")]
+        public string logradouro        { get; set; }
+        [JsonProperty("complemento")]
+        public string complemento       { get; set; }
+        [JsonProperty("bairro")]
+        public string bairro            { get; set; }
+        [JsonProperty("localidade")]
+        public string localidade        { get; set; }
+        [JsonProperty("uf")]
+        public string uf                { get; set; }
+        [JsonProperty("ibge")]
+        public string ibge              { get; set; }
+        [JsonProperty("gia")]
+        public string gia               { get; set; }
+        [JsonProperty("ddd")]
+        public string ddd               { get; set; }
+        [JsonProperty("siafi")]
+        public string siafi             { get; set; }
+    }
+}
---
+++ consumindo-api-rest-csharp/UI/JanelaPrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace consumindo_api_rest_csharp
+{
+    public partial class JanelaPrincipal : Form
+    {
+        public JanelaPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
