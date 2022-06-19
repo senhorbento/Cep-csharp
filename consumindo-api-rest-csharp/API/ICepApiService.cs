9a1c03d58faef72f80eed522eebf05bcf1966eee
senhorbento <sbento.ti@gmail.com>
2022-06-19 06:53:43 -0700

    Feat: Add interface ICepApiService

---
+++ consumindo-api-rest-csharp/API/ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace consumindo_api_rest_csharp.API
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<EntidadeCep> GetAddressAsync(string cep);
+    }
+}
