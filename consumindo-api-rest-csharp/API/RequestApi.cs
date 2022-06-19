80bec0080506cc2aba464f74bc026334887f44e5
senhorbento <sbento.ti@gmail.com>
2022-06-19 07:07:42 -0700

    Feat: Add RequestApi

---
+++ consumindo-api-rest-csharp/API/RequestApi.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace consumindo_api_rest_csharp.API
+{
+    public class RequestApi
+    {
+        public EntidadeCep address = new EntidadeCep();
+        public async Task Request(string cep)
+        {
+            try
+            {
+                var cepCliente = RestService.For<ICepApiService>("http://viacep.com.br");
+                address = await cepCliente.GetAddressAsync(cep);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro" + e);
+            }
+        }
+
+        public EntidadeCep GetEntidade(string cep)
+        {
+            _ = Request(cep);
+            return address;
+        }
+    }
+}
